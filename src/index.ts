c94467352bf54f3fc19bc25798665fe80a9ec228
Aykahshi <pshakya8751@gmail.com>
2026-01-23 11:46:27 +0800

    feat: bump package version

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const program = new Command();
 program
   .name("omo-switch")
   .description("CLI tool for managing oh-my-opencode profiles")
-  .version("0.1.0");
+  .version("0.1.1");
 
 program.addCommand(initCommand);
 program.addCommand(listCommand);
